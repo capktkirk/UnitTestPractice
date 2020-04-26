20241b2fc881b6bacbad31018cecf7aac84d5a7b
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 09:40:14 -0700

    Adding first sort test.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -21,13 +21,22 @@ TEST(PracticeTest, is_simple_palindrome)
     ASSERT_TRUE(actual);
     actual = obj.isPalindrome("aba");
     ASSERT_TRUE(actual);
-    actual = obj.isPalindrome("with spacesceaps htiw");
+    actual = obj.isPalindrome("wit h tiw");
     ASSERT_TRUE(actual);
 }
 
 TEST(PracticeTest, is_palindrome_with_symbols)
 {
 	Practice obj;
-	bool actual = obj.isPalindrome("..a..");
+	bool actual = obj.isPalindrome("!@#$%^&*()..a..)(*&^%$#@!");
 	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_simple_sort))
+{
+	Practice obj;
+	int num[3] = { 7, 5, 3 };
+	obj.sortDescending(num[0], num[1], num[2]);
+	ASSERT_LE(num[1], num[0]);
+	ASSERT_LE(num[2], num[1]);
 }
