5f29169439d0acf0b034859ed8404ffe8cf1594d
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 11:50:18 -0700

    Attempt to fix null character issues.

--- Practice.cpp
+++ Practice.cpp
@@ -1,5 +1,6 @@
 #include "Practice.h"
 #include <string>
+#include <boost/algorithm/string.hpp>
 
 using std::string;
 
@@ -32,7 +33,7 @@ void Practice::sortDescending(int & first, int & second, int & third)
 // character in the string, but disregarding case ('x' is the same as 'X')
 bool Practice::isPalindrome(string input)
 {
-  input.erase(std::find(input.begin(), input.end(), '\0', input.end()));
+  input.replace(std::find(input.begin(), input.end(), '\0', input.end()), '.');
   for(int i=0; i < input.size(); i++)
   {
     if( input[i] < 'A' || input[i] > 'Z' )
