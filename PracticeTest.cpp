7f60dd06b1d6e0415cc1fc224ce58fdbcfbbf6b9
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 09:26:05 -0700

    Obj revert.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -16,35 +16,7 @@ class PracticeTest : public ::testing::Test
 
 TEST(PracticeTest, is_simple_palindrome)
 {
-	Practice obj;
-	ASSERT_TRUE(obj.isPalindrome("aa"));
-	ASSERT_TRUE(obj.isPalindrome("tacocat"));
-	ASSERT_TRUE(obj.isPalindrome(".."));
-}
-
-TEST(PracticeTest, is_upper_lower_palindrome){
-	Practice obj;
-	bool actual = obj.isPalindrome("Aa");
-	ASSERT_TRUE(actual);
-	actual = obj.isPalindrome("Za");
-	ASSERT_FALSE(actual);
-	actual = obj.isPalindrome("TaCOcAt");
-	ASSERT_FALSE(actual);
-}
-
-
-TEST(PracticeTest, is_notAlpha){
-	Practice obj;
-	bool actual = obj.isPalindrome("./\\.");
-	ASSERT_FALSE(actual);
-}
-TEST(PracticeTest, is_Number){
-	Practice obj;
-	ASSERT_FALSE(obj.isPalindrome("1"));
-}
-
-TEST(PracticeTest, is_Not_palindrome){
-	Practice obj;
-	ASSERT_FALSE(obj.isPalindrome("ab"));
-	ASSERT_FALSE(obj.isPalindrome("Za"));
+    Practice obj;
+    bool actual = obj.isPalindrome("aa");
+    ASSERT_TRUE(actual);
 }
