b0ae2c53b2dd25f7290b5e8bb9eab7fcecda4894
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 09:43:51 -0700

    Adding first sort test.

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -39,4 +39,15 @@ TEST(PracticeTest, is_simple_sort))
 	obj.sortDescending(num[0], num[1], num[2]);
 	ASSERT_LE(num[1], num[0]);
 	ASSERT_LE(num[2], num[1]);
+	ASSERT_LE(num[2], num[0]);
+}
+
+Test(PracticeTest, is_sort_with_negative)
+{
+	Practice obj;
+	int num[4] = { 2, -1, -2 };
+	obj.sortDescending(num[0], num[1], num[2]);
+	ASSERT_LE(num[1], num[0]);
+	ASSERT_LE(num[2], num[1]);
+	ASSERT_LE(num[2], num[0]);
 }
