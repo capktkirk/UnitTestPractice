031b68f837e4442e19ab2242f03a713fa510587a
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 11:05:21 -0700

    Changed is_simple_sort 2

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -54,7 +54,7 @@ TEST(PracticeTest, is_simple_sort)
 	num[0] = -1;
 	num[1] = 0;
 	num[2] = 1;
-	obj.sortDescending(num[0], num[1], num2[]);
+	obj.sortDescending(num[0], num[1], num[2]);
 	ASSERT_LE(num[1], num[0]);
 	ASSERT_LE(num[2], num[1]);
 	ASSERT_LE(num[2], num[0]);
