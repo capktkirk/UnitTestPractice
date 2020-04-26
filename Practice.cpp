843b6103a8469b41d9e71c9c7991bba2ae0fe272
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-04-26 13:04:04 -0700

    Cout print 2

--- Practice.cpp
+++ Practice.cpp
@@ -1,6 +1,6 @@
 #include "Practice.h"
 #include <string>
-
+#include <iostream>
 using std::string;
 // using std::string::find;
 
@@ -40,7 +40,7 @@ bool Practice::isPalindrome(string input)
   //     input.erase(pos);
   //     pos = input.find('\0');
   // }
-  std::cout << "input == " << input << std::endl;
+  std::cout << "input == " << input << '\n';
 
   //input.erase(input.find('\0'));
   for(int i=0; i < input.size(); i++)
